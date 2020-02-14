f9ba2c94f30193d8027b97fb6fed7a4000d38bf2
beckham <beckham.lam@mail.mcgill.ca>
2020-02-13 21:44:58 -0500

    added angular material

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -3,13 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SiteCommonModule } from './components/site-common/site-common.module';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatExpansionModule} from '@angular/material/expansion';
 @NgModule({
   declarations: [
     DashboardComponent
   ],
   imports: [
     BrowserModule,
-    SiteCommonModule
+    SiteCommonModule,
+    MatTabsModule,
+    MatExpansionModule
   ],
   exports: [
     DashboardComponent
