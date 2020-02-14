73151efe2f36f8d2ac7598a9a7113a5cb3e95739
beckham <beckham.lam@mail.mcgill.ca>
2020-02-13 20:47:24 -0500

    footer and main page

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { SiteCommonModule } from './components/site-common/site-common.module';
 @NgModule({
   declarations: [
     DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SiteCommonModule
   ],
   exports: [
     DashboardComponent
