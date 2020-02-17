b8aba603128dc57ecadca7e420bd87c5e6d9f1f0
beckham <beckham.lam@mail.mcgill.ca>
2020-02-16 22:12:43 -0500

    added list item component

--- src/app/main/components/dashboard-common/dashboard-common.module.ts
+++ src/app/main/components/dashboard-common/dashboard-common.module.ts
@@ -5,11 +5,12 @@ import { MatButtonModule } from "@angular/material/button";
 import { TabGroupComponent } from "./tab-group/tab-group.component";
 import { MatTabsModule } from "@angular/material/tabs";
 import { InputListComponent } from "./input-list/input-list.component";
-import {MatListModule} from '@angular/material/list';
-import {MatInputModule} from '@angular/material/input';
-import { ReactiveFormsModule} from '@angular/forms';
+import { MatListModule } from "@angular/material/list";
+import { MatInputModule } from "@angular/material/input";
+import { ReactiveFormsModule } from "@angular/forms";
+import { ListItemComponent } from './list-item/list-item.component';
 @NgModule({
-  declarations: [TabGroupComponent, InputListComponent],
+  declarations: [TabGroupComponent, InputListComponent, ListItemComponent],
   imports: [
     BrowserModule,
     MatButtonModule,
