ba9b27cac2d6e6333c2e9a04cd27d8008a89b143
beckham <beckham.lam@mail.mcgill.ca>
2020-02-17 12:13:55 -0500

    feat(dashboard): added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './main/dashboard/dashboard.component';
+import { PageNotFoundComponent } from './main/page-not-found/page-not-found.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: DashboardComponent},
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { SiteCommonModule } from './components/site-common/site-common.module';
 import { MaterialCommonModule } from './components/material-common/material-common.module';
 import { DashboardCommonModule } from './components/dashboard-common/dashboard-common.module';
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
@@ -16,7 +18,8 @@ import { DashboardCommonModule } from './components/dashboard-common/dashboard-c
     DashboardCommonModule
   ],
   exports: [
-    DashboardComponent
+    DashboardComponent,
+    PageNotFoundComponent
   ],
   providers: [],
   bootstrap: [DashboardComponent]
