b3c1f7cf94bde95e07f47f27524fd690ae924fa5
beckham <beckham.lam@mail.mcgill.ca>
2020-02-18 20:48:04 -0500

    feat(preferences): stepper

---
+++ src/app/main/models/preferences_cuisine.model.ts
@@ -0,0 +1,9 @@
+export enum CuisineModel {
+    Asian = 'Asian',
+    Indian = 'Indian',
+    Japanese = 'Japanese',
+    Mexican = 'Mexican',
+    Greek = 'Greek',
+    French = 'French',
+    Italian = 'Italian'
+}
--- src/app/main/preferences/preferences-stepper/preferences-stepper.component.ts
+++ src/app/main/preferences/preferences-stepper/preferences-stepper.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { CuisineModel } from '../../models/preferences_cuisine.model';
 
 @Component({
   selector: 'preferences-stepper',
@@ -10,14 +11,24 @@ export class PreferencesStepperComponent implements OnInit {
   isLinear = false;
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
+  
+  cuisines = [
+    {key: CuisineModel.Asian, value: 'Asian'},
+    {key: CuisineModel.French, value: 'French'},
+    {key: CuisineModel.Greek, value: 'Greek'},
+    {key: CuisineModel.Indian, value: 'Indian'},
+    {key: CuisineModel.Italian, value: 'Italian'},
+    {key: CuisineModel.Japanese, value: 'Japanese'},
+    {key: CuisineModel.Mexican, value: 'Mexican'},
+  ]
 
-  constructor(private _formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
-    this.firstFormGroup = this._formBuilder.group({
+    this.firstFormGroup = this.formBuilder.group({
       firstCtrl: ['', Validators.required]
     });
-    this.secondFormGroup = this._formBuilder.group({
+    this.secondFormGroup = this.formBuilder.group({
       secondCtrl: ['', Validators.required]
     });
   }
